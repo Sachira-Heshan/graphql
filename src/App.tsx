0c818919542d608b2a7ba8be9b26cf86cf172e12
Sachira-Heshan <sachiraheshan99@gmail.com>
2022-12-27 00:01:14 +0530

    GraphQL

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
 import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+import { useEffect } from "react";
+
+export type Customer = {
+  id: number;
+  name: string;
+  industry: string;
+};
+
+const GET_DATA = gql`
+  {
+    customers {
+      id
+      name
+      industry
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_DATA);
+
+  useEffect(() => {
+    console.log(loading, error, data);
+  });
+
   return (
     <div className="App">
-      <p>Hello GraphQL</p>
+      {loading ? <p>Loading...</p> : null}
+      {error ? <p>Something went wrong!</p> : null}
+      {data
+        ? data.customers.map((customer: Customer) => {
+            return (
+              <p key={customer.name}>
+                {customer.name + " -> " + customer.industry}
+              </p>
+            );
+          })
+        : null}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+let client = new ApolloClient({
+  uri: "http://localhost:8000/graphql/",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      {
+        customers {
+          id
+          name
+          industry
+        }
+      }
+    `,
+  })
+  .then((result) => {
+    console.log(result);
+  });
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
